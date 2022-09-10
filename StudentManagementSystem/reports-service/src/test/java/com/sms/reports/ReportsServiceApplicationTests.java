280c295c1006d87c9de8b0cb708eeb18c1753835
Shaik Sirajuddin <shaik.sirajuddin01@ad.infosys.com>
2022-09-10 16:59:06 +0530

    SMSUI and Microservices
    Adding initial stage of developed application.

---
+++ StudentManagementSystem/reports-service/src/test/java/com/sms/reports/ReportsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sms.reports;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReportsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ StudentManagementSystem/sms-login/src/test/java/com/sms/login/SmsLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sms.login;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmsLoginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
